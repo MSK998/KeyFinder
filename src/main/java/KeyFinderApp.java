cbaf812716345caaeb1f52d07111fa6bbd1b660c
Mark <1602040@rgu.ac.uk>
2018-10-05 10:24:49 +0100

    changing the load method to accommodate for the 2d array that we will be using.

--- src/main/java/KeyFinderApp.java
+++ src/main/java/KeyFinderApp.java
@@ -13,56 +13,7 @@ import java.util.*;
 public class KeyFinderApp {
     public static void main(String[] args) {
 
-        //This is to test if the method works as there are no objects to test this on yet
-        try {
-            File myFile = new File("C:\\Users\\mwsco\\IdeaProjects\\KeyFinder\\src\\main\\resources\\Key Records Sample.xlsx");
-
-            FileInputStream fis = new FileInputStream(myFile);
-
-            //Finds the workbook instance for XLSX file
-            XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
-
-            //Return first sheet from the XLSX workbook
-            XSSFSheet sheetOne = myWorkBook.getSheetAt(0);
-
-            //Get iterator to move through all rows in the sheet
-            Iterator<Row> rowIterator = sheetOne.iterator();
-
-            //Traverse over the row of the XLSX file
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-
-                //For each row iterate through the columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
-
-                    Cell cell = cellIterator.next();
-
-                    switch (cell.getCellType()) {
-                        case STRING:
-                            System.out.println(cell.getStringCellValue() + "\t");
-                            break;
-                        case NUMERIC:
-                            System.out.println(cell.getNumericCellValue() + "\t");
-                            break;
-                        case BOOLEAN:
-                            System.out.println(cell.getBooleanCellValue() + "\t");
-                            break;
-                        case _NONE:
-                            System.out.println(cell.getStringCellValue() + "\t");
-                            break;
-
-                        default:
-                    }
-
-                }
-                System.out.println("");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-            System.exit(1);
-        }
-
+        KeyFinder sheet1 = new KeyFinder();
+        sheet1.loadData();
     }
 }
