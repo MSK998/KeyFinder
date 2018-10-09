a90d6ca6a2d9bfa78a27d7bbdb27b096363142f8
Mark <1602040@rgu.ac.uk>
2018-10-09 14:47:25 +0100

    Notes, Clean Up

--- src/main/java/KeyFinder/KeyWriter.java
+++ src/main/java/KeyFinder/KeyWriter.java
@@ -25,14 +25,18 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class KeyWriter {
     
   public static void main(String[] args) {
+      //states the file location, will change to a relative path in the project.
     String excelFilePath = ("/Users/Lewis/Desktop/University/CM3108/KeyRecordsSample.xlsx");
-    
+
     try {
+        //finds the file and sets as input stream
     FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+    //sets workbook and sheet
     Workbook workbook = WorkbookFactory.create(inputStream);
     
     Sheet sheet = workbook.getSheetAt(1);
-    
+
+    //test data
     Object[][] newKeys = {
         {2456, "Tambour unit", "Lewis", "N533"},
         {2245, "Tambour unit", "Jim", "N524"},
@@ -40,17 +44,21 @@ public class KeyWriter {
         {2459, "Door", "Ryan", "N128"},
             
     };
-    
+
+    //finds the amount of rows in the sheet
     int rowCount = sheet.getLastRowNum();
-    
+
+    //for the object row
     for (Object[] aKey : newKeys) {
         Row row = sheet.createRow(++rowCount);
-        
+
+        //column count set to 0
         int columnCount = 0;
         
         Cell cell = row.createCell(columnCount);
         cell.setCellValue(rowCount);
-        
+
+        //for each column
         for (Object field : aKey) {
             cell = row.createCell(++columnCount);
             if (field instanceof String) {
